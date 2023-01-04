7b87cfeaf3307d92d0f32d9c1d2feb4fd9922912
Sarah Mukuti <cesarwrites24@gmail.com>
2023-01-04 22:38:35 +0300

    modify solution

--- sumofnums.py
+++ sumofnums.py
@@ -1,3 +1,6 @@
+""""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+"""
 class Solution(object):
     def twoSum(nums, target):
         arr_len = len(nums)
